58e071c5ae714222cadb27948b4799959e729bde
SqixOW <king4995.tk@gmail.com>
2021-03-22 02:37:33 +0900

    # 210322 02:37 commit - devided handler -> handler / map information class / log struct class

--- logConverter.py
+++ logConverter.py
@@ -2,84 +2,9 @@ import re
 import sys
 import math
 import csv
+from logDataStruct import LogPattern, MatchInfo, PlayerData
 from dataclasses import dataclass, asdict
-
-@dataclass 
-class MatchInfo: #Information about current match (map, type, map section, team info)
-    Map: str = ''
-    MapType: str = ''
-    RoundName: str = ''
-    Section: str = ''
-    Team_1: str = ''
-    Team_2: str = ''
-    Offense: str = ''
-    Defense: str = ''
-
-@dataclass
-class PlayerData: #Player stat on csv
-    Map: str = ''
-    Section: str = ''
-    Point: str = '0'
-    RoundName: str = ''
-    Timestamp: str = ''
-    Team: str = ''
-    Player: str = ''
-    Hero: str = ''
-    HeroDamageDealt: str = '0'
-    BarrierDamageDealt: str = '0'
-    DamageBlocked: str = '0'
-    DamageTaken: str = '0'
-    Deaths: str = '0'
-    Eliminations: str = '0'
-    FinalBlow: str = '0'
-    EnvironmentalDeaths: str = '0'
-    EnvironmentalKills: str = '0'
-    HealingDealt: str = '0'
-    ObjectiveKills: str = '0'
-    SoloKills: str = '0'
-    UltimatesEarned: str = '0'
-    UltimatesUsed: str = '0'
-    HealingReceived: str = '0'
-    UltimateCharge: str = '0'
-    PlayerClosest: str = ''
-    Cooldown1: str = '0'
-    Cooldown2: str = '0'
-    Position: str = ''
-    MaxHealth: str = '0'
-    DeathByHero: str = ''
-    DeathByAbility: str = ''
-    DeathByPlayer: str = ''
-    Resurrected: str = ''
-    DuplicatedHero: str = ''
-    DuplicateStatus: str = ''
-
-class LogPattern: # Regex log patterns
-    def __init__(self):
-        self.pattern_dupstart = re.compile('(\[(.*?)\])\s(\d*\.?\d+),(DuplicatingStart),(\w*),(\w*)')
-        self.pattern_dupend = re.compile('(\[(.*?)\])\s(\d*\.?\d+),(DuplicatingEnd),(\w*)')
-        self.pattern_resurrect = re.compile('(\[(.*?)\])\s(\d*\.?\d+),(Resurrected),(\w*)')
-        self.pattern_finalblow = re.compile('(\[(.*?)\])\s(\d*\.?\d+),(FinalBlow),(\w*),(\w*),(\w*\s*\w*)')
-        self.pattern_suicide = re.compile('(\[(.*?)\])\s(\d*\.?\d+),(Suicide),(\w*)')
-        self.pattern_matchInfo = re.compile('(\[(.*?)\])\s(\w*\s*\w*\s*\w*|Watchpoint: Gibraltar|King\'s Row),(\w*\s*\w*),(\w*\s*\w*),(\d)')
-        self.pattern_playerInfo = re.compile('(\[(.*?)\])\s(\w*),(\w*),(\w*),(\w*),(\w*),(\w*),(\w*),(\w*),(\w*),(\w*),(\w*),(\w*)')
-        self.pattern_typeControl = re.compile('(\[(.*?)\])\s(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+)')
-        self.pattern_typeOthers = re.compile('(\[(.*?)\])\s(\d*\.?\d+),(True|False),(\d*\.?\d+)')
-        self.pattern_playerData = re.compile('(\[(.*?)\])\s(\d*\.?\d+),(\w*),(\w*\s*\w*|Soldier: 76|D.Va),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+),(\w*),(\W[-]?(\d*\.?\d+), [-]?(\d*\.?\d+), [-]?(\d*\.?\d+)\W),(\w*\s*\w*),(\d*\.?\d+),(\d*\.?\d+),(\d*\.?\d+)')
-
-class Controls: # Control map subtitles
-    def __init__(self):
-        self.ilios = ['Lighthouse','Well','Ruins']
-        self.lijiang_tower = ['Night Market','Garden','Control Tower']
-        self.nepal = ['Village', 'Shrine', 'Sanctum']
-        self.oasis = ['City Center', 'Gardens', 'University']
-        self.busan = ['Sanctuary', 'Downtown', 'Meka Base']
-
-class Maps: # Map types and the name of maps
-    def __init__(self):
-        self.control = ['Ilios', 'Lijiang Tower', 'Nepal', 'Oasis', 'Busan']
-        self.assault = ['Horizon Lunar Colony', 'Temple of Anubis', 'Volskaya Industries', 'Paris', 'Hanamura']
-        self.hybrid = ['King\'s Row', 'Eichenwalde', 'Numbani', 'Hollywood', 'Blizzard World']
-        self.escort = ['Route 66', 'Watchpoint: Gibraltar', 'Dorado', 'Rialto', 'Havana', 'Junkertown']
+from mapInfo import Controls, Maps
         
 class LogHandler: # Log Parsing & Handling
     def __init__(self, args):
---
+++ mapInfo.py
@@ -0,0 +1,14 @@
+class Controls: # Control map subtitles
+    def __init__(self):
+        self.ilios = ['Lighthouse','Well','Ruins']
+        self.lijiang_tower = ['Night Market','Garden','Control Tower']
+        self.nepal = ['Village', 'Shrine', 'Sanctum']
+        self.oasis = ['City Center', 'Gardens', 'University']
+        self.busan = ['Sanctuary', 'Downtown', 'Meka Base']
+
+class Maps: # Map types and the name of maps
+    def __init__(self):
+        self.control = ['Ilios', 'Lijiang Tower', 'Nepal', 'Oasis', 'Busan']
+        self.assault = ['Horizon Lunar Colony', 'Temple of Anubis', 'Volskaya Industries', 'Paris', 'Hanamura']
+        self.hybrid = ['King\'s Row', 'Eichenwalde', 'Numbani', 'Hollywood', 'Blizzard World']
+        self.escort = ['Route 66', 'Watchpoint: Gibraltar', 'Dorado', 'Rialto', 'Havana', 'Junkertown']
